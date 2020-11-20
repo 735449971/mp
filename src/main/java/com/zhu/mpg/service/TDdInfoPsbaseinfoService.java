8526e33f7bda69f2ae4802d5a95c7aa5b44c3083
Wangweiping <953225549@qq.com>
2020-11-20 14:42:57 +0800

    11

---
+++ src/main/java/com/zhu/mpg/service/TDdInfoPsbaseinfoService.java
@@ -0,0 +1,16 @@
+package com.zhu.mpg.service;
+
+import com.zhu.mpg.entity.TDdInfoPsbaseinfo;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ * 驻厂调度信息表 服务类
+ * </p>
+ *
+ * @author wxl
+ * @since 2020-11-20
+ */
+public interface TDdInfoPsbaseinfoService extends IService<TDdInfoPsbaseinfo> {
+
+}
